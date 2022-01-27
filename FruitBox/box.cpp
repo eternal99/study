7ff62d2e60cb491345cb25dec70b709aab483673
ETERNAL <fldzm7905@gmail.com>
2022-01-27 15:13:27 +0900

    Fix list in box.cpp

--- FruitBox/box.cpp
+++ FruitBox/box.cpp
@@ -2,20 +2,23 @@
 #include <iostream>
 using namespace std;
 Box::Box(string name)
-	:m_name(name)
+	: m_name(name)
 {
 	//m_name = name;
-	//stuff = new list<Apple *>;
+	stuff = new list<Apple *>;
 }
 
-void Box::Add(Apple * r){
+void Box::Add(Apple *r)
+{
 	stuff->push_back(r);
 }
 
-void Box::Show(){
+void Box::Show()
+{
 	list<Apple *>::iterator i;
 	cout << "상자. 이름은: " << m_name << ". 안에 들은 물건은: " << endl;
-	for (i=stuff->begin(); i != stuff->end(); i++){
+	for (i = stuff->begin(); i != stuff->end(); i++)
+	{
 		(*i)->Show();
 	}
 	cout << m_name << " 끝 ]" << endl;
